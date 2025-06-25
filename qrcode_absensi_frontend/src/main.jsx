8e778486e2888b0f4d2e5628fd6b789e615c4228
torikallcode <torikalcontent@gmail.com>
2025-06-25 10:36:05 +0800

    update dependenci

--- qrcode_absensi_frontend/src/main.jsx
+++ qrcode_absensi_frontend/src/main.jsx
@@ -4,13 +4,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ScanPage from "./pages/ScanPage.jsx";
 import QrGenerator from "./pages/QRGenerator.jsx";
 import "./index.css";
+import Dashboard from "./pages/Dashboard.jsx";
+import AttendanceList from "./pages/AttendanceList.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ScanPage />} />
+        <Route path="/scan" element={<ScanPage />} />
         <Route path="/qr-generator" element={<QrGenerator />} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/att" element={<AttendanceList />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
